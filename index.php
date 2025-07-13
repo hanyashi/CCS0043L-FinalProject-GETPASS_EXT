47f84cd5063f0123446c9910ea27b81e39674334
Nathan0721 <nathan.alisangco@gmail.com>
2025-07-13 18:55:46 +0800

    Create launch page HTML & CSS

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Launch Page</title>
+    <link rel="icon" href="assets/images/getpass-icon.png">
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+<body>
+    <div class="background">
+    </div>
+    <div class="main">
+        <h1 class="header">GET<i>PASS</i> - EZ<i>RIDE</i></h1>
+        <img src="assets/images/getpass-icon.png" class="logo">
+        <div class="button-container">
+            <button>Continue</button>
+            <button>Download App</button>
+        </div>
+    </div>
+</body>
+</html>
