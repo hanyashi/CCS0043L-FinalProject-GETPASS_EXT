d47d31e826ebe642e8997a1feb3258609bf4602f
Nathan0721 <nathan.alisangco@gmail.com>
2025-07-13 20:27:40 +0800

    Upd: Adjust size of elements in passengers page and organize files in directory

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
         <h1 class="header">GET<i>PASS</i> - EZ<i>RIDE</i></h1>
         <img src="assets/images/getpass-icon.png" class="logo">
         <div class="button-container">
-            <a href="passengers_page.php"><button>Continue</button></a>
-            <a href=""><button>Download App</button></a>
+            <a href="pages/passengers_page.php"><button>Continue</button></a>
+            <a href="https://play.google.com/store/apps/details?id=com.getevee.getpass&hl=en&pli=1" target="_blank"><button>Download App</button></a>
         </div>
     </div>
 </body>
--- pages/passengers_page.php
+++ pages/passengers_page.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/passengers_page.css">
+    <link rel="stylesheet" href="../assets/css/passengers_page.css">
     <title>Passengers Page</title>
+    <link rel="icon" href="../assets/images/getpass-icon.png">
 </head>
 <body>
     <div class="background">
@@ -12,23 +13,23 @@
 
     <div class="main">
         <div class="navbar">
-            <a href="index.php">GET<i>PASS</i> - EZ<i>RIDE</i> </a>
+            <a href="../index.php">GET<i>PASS</i> - EZ<i>RIDE</i> </a>
         </div>
 
         <div class="passenger">
             <h1>Amount of Passengers</h1>
             <div class="passenger-cards">
                 <button class="card">
-                    <img src="assets/images/1_person_icon.png" alt="1person">
+                    <img src="../assets/images/1_person_icon.png" alt="1 Person">
                 </button>
                 <button class="card">
-                    <img src="assets/images/2_person_icon.png" alt="1person">
+                    <img src="../assets/images/2_person_icon.png" alt="2 People">
                 </button>
                 <button class="card">
-                    <img src="assets/images/3_person_icon.png" alt="1person">
+                    <img src="../assets/images/3_person_icon.png" alt="3 People">
                 </button>
             </div>
-            <a href=""><button>Proceed to Payment</button></a>
+            <a href="payment.php"><button>Proceed to Payment</button></a>
         </div>
     </div>
 </body>
